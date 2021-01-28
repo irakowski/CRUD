a0eaf6d6c2abb8a8ec90d8bc4183cbd2b4b719e7
irakowski <inna.rakowski@gmail.com>
2021-01-28 21:32:58 +0100

    Register Model in Admin

--- jboffer/admin.py
+++ jboffer/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from jboffer.models import Company, ApplicationTag, MyApplication
+
+admin.site.register(Company)
+admin.site.register(ApplicationTag)
+admin.site.register(MyApplication)
 
 # Register your models here.
