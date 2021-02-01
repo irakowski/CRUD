2af52201ac54d6c05aea62c57a1b0ffdd0fcbee5
irakowski <inna.rakowski@gmail.com>
2021-02-01 23:20:42 +0100

    Ajax delete

--- jboffer/static/jboffer/ajax.js
+++ jboffer/static/jboffer/ajax.js
@@ -12,6 +12,7 @@ function update_app(url) {
         // handle a successful response
         success : function(data) {
             $('#edit-form').css("display","none")/// hide the form
+            $('display').html(data)
             location.reload(); //fetch updated item and render page again
         },
 
@@ -24,9 +25,37 @@ function update_app(url) {
     });
 };
 
+// Submit post on submit
+function delete_app(url) {
+    $.ajax({
+        url : url, // the endpoint
+        type : "POST", // http method
+        data : { csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken]").val() }, // data sent with the post request
+
+        // handle a successful response
+        success : function(data) {
+            window.location = '/'/// hide the form
+            ; //fetch updated item and render page again
+        },
+
+        // handle a non-successful response
+        error : function(xhr,errmsg, err) {
+            $('#results').html("<div class='alert-box alert radius' data-alert>Oops! We have encountered an error: "+errmsg+
+                " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
+            console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+        }
+    });
+};
 
 $('#act-form').on('submit', function(e){
     e.preventDefault();
     console.log("form submitted!")
     update_app($(this).attr("data-href"));
+});
+
+
+$('#del-form').on('submit', function(e){
+    e.preventDefault();
+    console.log("sending delete!")
+    delete_app($(this).attr("data-href"));
 });
