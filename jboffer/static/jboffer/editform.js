e164b8756a52654031236aa2bfd9b56c3ef906e9
irakowski <inna.rakowski@gmail.com>
2021-02-01 23:20:16 +0100

    Js form events

--- jboffer/static/jboffer/editform.js
+++ jboffer/static/jboffer/editform.js
@@ -3,15 +3,28 @@ document.addEventListener("DOMContentLoaded", function() {
     
     const showEditIcon = document.getElementById('show-edit')
     const editForm = document.getElementById('edit-form');
+    const deleteIcon = document.getElementById("show-delete");
+    const deleteForm = document.getElementById('delete-form');
     
+
     showEditIcon.addEventListener('click', function(e) {
         e.preventDefault();
         editForm.style.display = editForm.style.display === 'none' ? 'block' : 'none';
+    })
+    deleteIcon.addEventListener('click', function(e) {
+        e.preventDefault();
+        deleteForm.style.display = deleteForm.style.display === 'none' ? 'block' : 'none';
+    })
     
     const discardButton = document.getElementById('discard-edit');
     discardButton.addEventListener('click', function(e) {
         e.preventDefault();
-        editForm.style.display = 'none'
-        })
-    });    
+        editForm.style.display = 'none';
+      }) 
+
+    const discardDelete = document.getElementById('discard-delete');
+    discardDelete.addEventListener('click', function(e) {
+        e.preventDefault();
+        deleteForm.style.display = 'none';
+      })
 });
