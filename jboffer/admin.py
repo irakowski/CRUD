fee03bf47628b24135abb8cf6c916d77e225fde9
irakowski <inna.rakowski@gmail.com>
2021-02-02 22:25:54 +0100

    Register JoOfferTable in admin

--- jboffer/admin.py
+++ jboffer/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from jboffer.models import Company, ApplicationTag, MyApplication
+from jboffer.models import Company, ApplicationTag, MyApplication, JobOffer
 
 admin.site.register(Company)
 admin.site.register(ApplicationTag)
 admin.site.register(MyApplication)
+admin.site.register(JobOffer)
 
 # Register your models here.
