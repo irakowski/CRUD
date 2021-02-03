449fc481afffb520aa202f40cf0d2429e33b569f
irakowski <inna.rakowski@gmail.com>
2021-02-03 20:26:34 +0100

    SingleOfferRouting/cleanup

--- ojom/urls.py
+++ ojom/urls.py
@@ -26,13 +26,13 @@ urlpatterns = [
     path('company/new', general_views.CreateCompany.as_view(), name='create-company'),
     path('companies/', general_views.CompanyListView.as_view(), name='company-list'),
     path('offers', general_views.JobOfferListView.as_view(), name='job-offer-list'),
-    path('offer/<pk>',general_views.JobOfferDetailView, name='view-offer'),
+    path('offer/<int:pk>',general_views.JobOfferDetailView.as_view(), name='view-offer'),
     
     path('application/<str:tag>', general_views.ApplicationsByTag.as_view(), name='app-tags'),
     path('stats/',general_views.Stats.as_view(), name='stats'),
 
-    path('ajax/update/<pk>', general_views.ajax_update, name='update-app'),
-    path('ajax/delete/<pk>', general_views.ajax_delete, name='delete-app'),
+    path('ajax/update/<int:pk>', general_views.ajax_update, name='update-app'),
+    path('ajax/delete/<int:pk>', general_views.ajax_delete, name='delete-app'),
     path('admin/', admin.site.urls),
 
     
